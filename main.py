b6137c417dce44c3c6c67b2f45ab2e3b284a39b7
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-06-29 16:16:57 -0300

    A small updated in the Main class

--- main.py
+++ main.py
@@ -7,29 +7,29 @@
 import second_degree_equation;
 
 class Main:
-    if __name__ == '__main__':
-        print("This application solves a second degree equation.");
+    print("This application solves a second degree equation.");
 
-        def menu(self):
-            print("\n****************************Options****************************\n");
-            print("1 - Solve a new second degree equation");
-            print("2 - Exit");
+    def menu(self):
+        print("\n****************************Options****************************\n");
+        print("1 - Solve a new second degree equation");
+        print("2 - Exit");
 
-        def solveEquation(self):
-            a = int(input("Enter a value for a: "));
-            b = int(input("Enter a value for b: "));
-            c = int(input("Enter a value for c: "));
-            equation = second_degree_equation.Second_degree_equation(a, b, c);
-            equation.calculateSecondDegreeEquation();
+    def solveEquation(self):
+        a = int(input("Enter a value for a: "));
+        b = int(input("Enter a value for b: "));
+        c = int(input("Enter a value for c: "));
+        equation = second_degree_equation.Second_degree_equation(a, b, c);
+        equation.calculateSecondDegreeEquation();
 
-equation = Main();
-equation.menu();
-
-option = int(input("Choose an option: "));
-while option == 1:
-    equation.solveEquation();
+if __name__ == '__main__':
+    equation = Main();
     equation.menu();
-    option = int(input("Choose an option: "));
 
-if option == 2:
-    print("End program.");
+    option = int(input("Choose an option: "));
+    while option == 1:
+        equation.solveEquation();
+        equation.menu();
+        option = int(input("Choose an option: "));
+
+    if option == 2:
+        print("End program.");
