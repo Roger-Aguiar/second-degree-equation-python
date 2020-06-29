2841448a24bdf28c0959e49176942d711266890b
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-06-29 15:37:38 -0300

    Calculus for delta equal 0 and delta less than 0 implemented.

--- second_degree_equation.py
+++ second_degree_equation.py
@@ -1,7 +1,7 @@
 # Name:         Roger Silva Santos Aguiar
 # Function:     This module solves a complete second-degree equation.
 # Initial date: June 28, 2020
-# Last update:  June 28, 2020
+# Last update:  June 29, 2020
 
 # Required modules
 import math;
@@ -14,17 +14,31 @@ class Second_degree_equation:
 
     def calculateSecondDegreeEquation(self):
         delta = (self.b * self.b) - (4 * self.a * self.c);
-        x1 = (-(self.b) + math.sqrt(delta)) / (2 * self.a);
-        x2 = (-(self.b) - math.sqrt(delta)) / (2 * self.a);
 
-        x1 = int(x1);
-        x2 = int(x2);
+        if delta > 0:
+            x1 = (-(self.b) + math.sqrt(delta)) / (2 * self.a);
+            x2 = (-(self.b) - math.sqrt(delta)) / (2 * self.a);
+            x1 = int(x1);
+            x2 = int(x2);
 
-        print("\n*************************Result*************************\n");
+            print("\n*************************Result*************************\n");
 
-        print("The value of delta = {} " .format(delta));
-        print("The value of X1 = {} " .format(x1));
-        print("The value of X2 = {} " .format(x2));
+            print("The value of delta = {} " .format(delta));
+            print("The value of X1 = {} " .format(x1));
+            print("The value of X2 = {} " .format(x2));
+        elif delta == 0:
+            x1 = (-(self.b) + math.sqrt(delta)) / (2 * self.a);
+            x1 = float(x1);
+
+            print("\n*************************Result*************************\n");
+
+            print("The value of delta = {} ".format(delta));
+            print("The value of X1 = {} ".format(x1));
+        else:
+            print("\n*************************Result*************************\n");
+
+            print("The value of delta = {} ".format(delta));
+            print("No real root. Delta is less than 0.");
 
 if __name__ == '__main__':
     a = int(input("Enter a value for a: "));
